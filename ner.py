5fbdbc14d0de381238225df09ac8e10ba51700b3
Kashish Gupta <kashishg@wharton.upenn.edu>
2018-03-23 01:17:17 -0400

    created pipeline

--- ner.py
+++ ner.py
@@ -1,9 +1,9 @@
-from nltk.corpus import conll2002
+#from nltk.corpus import conll2002
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import Perceptron
 from sklearn.neural_network import MLPClassifier
 import re
-from syllables import count_syllables
+#from syllables import count_syllables
 from sklearn.metrics import precision_recall_fscore_support
 
 # Assignment 7: NER
@@ -22,9 +22,9 @@ def getfeats(word, o):
 
     ]
     return features
-    
 
-def word2features(sent, i):
+
+def word2features(triplet, i):
     """ The function generates all features
     for the word at position i in the
     sentence."""
@@ -35,27 +35,28 @@ def word2features(sent, i):
             word = sent[i+o][0]
             featlist = getfeats(word, o)
             features.extend(featlist)
-    
+
     return dict(features)
 
 if __name__ == "__main__":
     # Load the training data
-    train_lines = open('data')
-    dev_sents = list(conll2002.iob_sents('esp.testa'))
-    test_sents = list(conll2002.iob_sents('esp.testb'))
-    
+    with open('bless2011/data_lex_train.tsv', 'r') as f:
+        inputlines = f.read().strip().split('\n')
+    inputlines = [[s.split("\t")] for s in inputlines]
+    #train_lines = open('data')
+    #dev_sents = list(conll2002.iob_sents('esp.testa'))
+    #test_sents = list(conll2002.iob_sents('esp.testb'))
+
     train_feats = []
     train_labels = []
 
-    for tup in train_lines:
+    for triplet in inputlines:
+        feats = word2features(triplet)
+        train_feats.append(feats)
+        train_labels.append(triplet[2])
 
-    '''
-    for sent in train_sents:
-        for i in range(len(sent)):
-            feats = word2features(sent,i)
-            train_feats.append(feats)
-            train_labels.append(sent[i][-1])
-    '''
+    #this is where ive stopped
+    
     vectorizer = DictVectorizer()
     X_train = vectorizer.fit_transform(train_feats)
 
@@ -80,7 +81,7 @@ if __name__ == "__main__":
     print("Writing to results.txt")
     # format is: word gold pred
     with open("results.txt", "w") as out:
-        for sent in dev_sents: 
+        for sent in dev_sents:
             for i in range(len(sent)):
                 word = sent[i][0]
                 gold = sent[i][-1]
